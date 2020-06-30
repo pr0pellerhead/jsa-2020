deda143af7734c19ab679555e907ff8c6f75d72d
Bojan Gavrovski <bojan@beyondbasics.co>
2020-06-30 22:06:21 +0200

    in memory data

--- c1/handlers/index.js
+++ c1/handlers/index.js
@@ -1,23 +1,38 @@
+const students = [
+    {fname: 'Pero', lname: 'Perovski', gpa: 9.2},
+    {fname: 'Janko', lname: 'Jankovski', gpa: 7.0},
+    {fname: 'Stanko', lname: 'Stankovski', gpa: 6.1},
+];
+
 const getAllStudents = (req, res) => {
-    res.status(200).send('ok');
+    return res.status(200).send(students);
 };
 
 const getSingleStudent = (req, res) => {
-    res.status(200).send('ok');
+    if(students[req.params.id] !== undefined){
+        return res.status(200).send(students[req.params.id]);
+    }
+    return res.status(404).send('Not Found');
 };
 
 const createStudent = (req, res) => {
-    res.status(200).send('ok');
+    if(req.body){
+        students.push(req.body);
+        return res.status(201).send('Created');
+    }
+    return res.status(400).send('Bad request');
 };
 
 const removeStudent = (req, res) => {
     res.status(200).send('ok');
 };
 
+// update needs the full student object
 const updateStudent = (req, res) => {
     res.status(200).send('ok');
 };
 
+// patch needs whatever we give it to change
 const patchStudent = (req, res) => {
     res.status(200).send('ok');
 };
--- c1/index.js
+++ c1/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const handlers = require('./handlers');
 
 const api = express();
+api.use(bodyParser.json());
 
 api.get('/students', handlers.getAllStudents);
 api.get('/students/:id', handlers.getSingleStudent);
