24f3d01b2518a21abedbad4b640bef3e1086c834
Bojan Gavrovski <bojang@gmail.com>
2020-07-02 20:34:55 +0200

    finished CRUD

--- c1/handlers/index.js
+++ c1/handlers/index.js
@@ -1,7 +1,7 @@
-const students = [
-    {fname: 'Pero', lname: 'Perovski', gpa: 9.2},
-    {fname: 'Janko', lname: 'Jankovski', gpa: 7.0},
-    {fname: 'Stanko', lname: 'Stankovski', gpa: 6.1},
+var students = [
+    {id: 1, fname: 'Pero', lname: 'Perovski', gpa: 9.2},
+    {id: 2, fname: 'Janko', lname: 'Jankovski', gpa: 7.0},
+    {id: 3, fname: 'Stanko', lname: 'Stankovski', gpa: 6.1},
 ];
 
 const getAllStudents = (req, res) => {
@@ -17,24 +17,44 @@ const getSingleStudent = (req, res) => {
 
 const createStudent = (req, res) => {
     if(req.body){
-        students.push(req.body);
+        students.push({
+            ...req.body,
+            id: students[students.length - 1].id + 1
+        });
         return res.status(201).send('Created');
     }
     return res.status(400).send('Bad request');
 };
 
 const removeStudent = (req, res) => {
-    res.status(200).send('ok');
+    students = students.filter((e, i) => {
+        return e.id != parseInt(req.params.id);
+    });
+    res.status(204).send();
 };
 
 // update needs the full student object
 const updateStudent = (req, res) => {
-    res.status(200).send('ok');
+    students = students.map((s) => {
+        if(s.id === parseInt(req.params.id)) {
+            let d = {...req.body, id: parseInt(req.params.id)}
+            return  d;
+        }
+        return s;
+    });
+    res.status(204).send();
 };
 
 // patch needs whatever we give it to change
 const patchStudent = (req, res) => {
-    res.status(200).send('ok');
+    students = students.map((s) => {
+        if(s.id === parseInt(req.params.id)) {
+            let d = {...s, ...req.body, id: parseInt(req.params.id)}
+            return  d;
+        }
+        return s;
+    });
+    res.status(204).send();
 };
 
 module.exports = {
